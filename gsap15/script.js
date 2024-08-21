f7d0571a76aa9860e8166604e0faf05f8709b82b
iNa. <aljard@naver.com>
2024-08-21 23:24:57 +0900

    GSAP 스무스 효과

---
+++ gsap15/script.js
@@ -0,0 +1,12 @@
+const lenis = new lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
